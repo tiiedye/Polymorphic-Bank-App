087c59229e53e25d9135ab68af425778d88893f8
tiiedye <tiiedye@gmail.com>
2020-09-10 11:52:41 -0700

    creating files and adding source code

--- Bank-App/Bank-App/main.cpp
+++ Bank-App/Bank-App/main.cpp
@@ -1,20 +1,70 @@
-// Bank-App.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
-
 #include <iostream>
+#include <vector>
+#include "Account.h"
+#include "Savings_Account.h"
+#include "Checking_Account.h"
+#include "Trust_Account.h"
+#include "Account_Util.h"
 
-int main()
-{
-    std::cout << "Hello World!\n";
+using namespace std;
+
+int main() {
+    cout.precision(2);
+    cout << fixed;
+
+    // Accounts
+    vector<Account> accounts;
+    accounts.push_back(Account{});
+    accounts.push_back(Account{ "Larry" });
+    accounts.push_back(Account{ "Moe", 2000 });
+    accounts.push_back(Account{ "Curly", 5000 });
+
+    display(accounts);
+    deposit(accounts, 1000);
+    withdraw(accounts, 2000);
+
+    // Savings 
+
+    vector<Savings_Account> sav_accounts;
+    sav_accounts.push_back(Savings_Account{});
+    sav_accounts.push_back(Savings_Account{ "Superman" });
+    sav_accounts.push_back(Savings_Account{ "Batman", 2000 });
+    sav_accounts.push_back(Savings_Account{ "Wonderwoman", 5000, 5.0 });
+
+    display(sav_accounts);
+    deposit(sav_accounts, 1000);
+    withdraw(sav_accounts, 2000);
+
+    // Checking
+
+    vector<Checking_Account> check_accounts;
+    check_accounts.push_back(Checking_Account{});
+    check_accounts.push_back(Checking_Account{ "Kirk" });
+    check_accounts.push_back(Checking_Account{ "Spock", 2000 });
+    check_accounts.push_back(Checking_Account{ "Bones", 5000 });
+
+    display(check_accounts);
+    deposit(check_accounts, 1000);
+    withdraw(check_accounts, 2000);
+
+    // Trust
+
+    vector<Trust_Account> trust_accounts;
+    trust_accounts.push_back(Trust_Account{});
+    trust_accounts.push_back(Trust_Account{ "Athos", 10000, 5.0 });
+    trust_accounts.push_back(Trust_Account{ "Porthos", 20000, 4.0 });
+    trust_accounts.push_back(Trust_Account{ "Aramis", 30000 });
+
+    display(trust_accounts);
+    deposit(trust_accounts, 1000);
+    withdraw(trust_accounts, 3000);
+
+    // Withdraw 5 times from each trust account
+    // All withdrawals should fail if there are too many withdrawals or if the withdrawl is > 20% of the balance
+    for (int i = 1; i <= 5; i++)
+        withdraw(trust_accounts, 1000);
+
+
+
+    return 0;
 }
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
